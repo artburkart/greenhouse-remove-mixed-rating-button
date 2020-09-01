98c2ee715c2685ea419cd6a339ded1887820c025
Arthur Burkart <arthur@presynce.com>
2020-09-01 14:12:31 -0400

    Removes the "mixed-rating" button from the UI

---
+++ greenhouse.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Remove "Mixed Rating" Option
+// @namespace    presynce.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       artburkart
+// @match        https://*.greenhouse.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelectorAll("span.selectable.mixed-rating.rating-icon").forEach(e => e.parentNode.removeChild(e))
+    // Your code here...
+})();
